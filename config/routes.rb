77f676c74bd01e8440bde6cb9dc78b1f7ad02423
punisher49 <aliyev.hasanagha@gmail.com>
2020-01-16 16:49:14 +1100

    fixed broken css

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-root :to => "session#new"  
+root :to => "cocktails#index"
 
 
-  get 'cocktails/map'
-  get 'pages/map'
+get 'cocktails/map'
+get 'pages/map'
 get '/login' => 'session#new'
 post '/login' => 'session#create'
 delete '/login' => 'session#destroy'
