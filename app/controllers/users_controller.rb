eca7eb451d184233935cb96988a3f164dad2f1e2
punisher49 <aliyev.hasanagha@gmail.com>
2020-01-16 14:21:50 +1100

    fixed user update in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,7 +56,7 @@ class UsersController < ApplicationController
   end
 
   def update
-
+    @user = User.find params[:id]
     if @user.update(user_params)
       redirect_to @user, notice: "Your profile has been updated"
     else
